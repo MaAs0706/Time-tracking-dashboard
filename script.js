0304de16157d2c8a90e57b0483acffd3ad7c07ac
MaAs0706e <aswanthmadhav07@gmail.com>
2025-06-21 09:39:39 +0530

    feat: add update function

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 fetch('data.json')
 .then(response => response.json())
 .then (data =>{
-  const periodbutton = document.querySelectorAll('.period-btn');
+  const periodbutton = document.querySelectorAll('.period-btn')
   const activitycards = document.querySelectorAll('.activity-card')  
 
 
   let currentperiod = "daily"
 
-  function update(period){
+  function update(period){ 
+    activitycards.forEach(function(card) {
+      const title = card.querySelector('h2').textContent
+      const activity = data.find(function(item){
+        return item.title === title 
+      })
+
+    })
 
     
   }
